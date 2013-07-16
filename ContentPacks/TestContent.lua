687e611e602f4b7b5b58969d4cadc512e0f1b778
DigiTechs <tristanstaszko@hotmail.co.uk>
2013-07-16 17:04:10 +0100

    Update TestContent.lua

--- ContentPacks/TestContent.lua
+++ ContentPacks/TestContent.lua
@@ -1,4 +1,3 @@
 return setfenv(1,{
-["Hey!"] = {"http://www.lua.org/images/lua.gif", "lua", "gif"},
-["MusicTest"] = {"https://dl-web.dropbox.com/get/Cool%20music.wav?w=AABH_3IvGDI6Pe6wud868-36NQkMRYwGTQVcqV3SteKvpA&dl=1", "music", "wav"}
+["Hey!"] = {"http://www.lua.org/images/lua.gif", "lua", "gif"};
 })
